10f27c38bde290d613d94c85db55231f2d865be0
norbert-playing <136804788+norbert-playing@users.noreply.github.com>
2023-07-28 19:31:27 +0200

    with voice needs correction to go back on page

--- app/slownik/page.tsx
+++ app/slownik/page.tsx
@@ -9,16 +9,43 @@ import { deleteRecordAction, getUserId } from "../_action";
 import Button from "../components/Button";
 import { getServerSession } from "next-auth";
 import { authoption } from "@/lib/authoptions";
+import { Result } from "postcss";
+import Link from "next/link";
 
 function update() {
   console.log('record editable');
 }
+let dzwiek:string;
+async function getTranslation() {
+  
+  const url:string = 'https://api.dictionaryapi.dev/api/v2/entries/en/gray';
+  const options = {
+    method: 'GET',
+    // headers: {
+    //   'X-RapidAPI-Key': 'ceb5cf0e6bmsh7d73f02392d4529p192c24jsn3d857ee782a9',
+    //   'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+    // }
+  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    // const phonet = await result['phonetics']
+    console.log(result[0].phonetics[1].audio);
+    dzwiek = result[0].phonetics[1].audio as string
+    // return dzwiek
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const Slownik = async () => {
+  const getTrnsl = getTranslation()
   const session = await getServerSession(authoption)
   if(session){
     const email = session.user?.email as string
     const userid =await getUserId(email) as string
     const { records } = await getRecords(userid);
+
 
     return (
     <>
@@ -34,6 +61,7 @@ const Slownik = async () => {
         })}
       </ul>
       <Button />
+      <Link href={dzwiek}>dzwiek</Link>
     </>
   );
 };
